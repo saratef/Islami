7c8a5342829fbb419af0263b6ec3a12d550de46b
SaraAtef4 <saraatef396@gmail.com>
2026-07-17 21:23:09 +0300

    Revert "Complete Hadeth tab."
    This reverts commit 91e233b8aa0ede00d2aae43771f545a264f164e7.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.onBoardingRouteName,
+      initialRoute: AppRoutes.homeRouteName,
       routes: {
         AppRoutes.homeRouteName: (context) => HomeScreen(),
         AppRoutes.onBoardingRouteName: (context) => OnBoardingPage(),

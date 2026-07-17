d33473dc2a7d39c2ec0cfe886db6e6dabf69e522
SaraAtef4 <saraatef396@gmail.com>
2026-07-17 21:22:23 +0300

    Revert "Complete Hadeth tab."
    This reverts commit 09392a8af34f9a37087896cfa7b8d3981c6bcd73.

--- lib/ui/home/tabs/hadeth/hadeth_tab.dart
+++ lib/ui/home/tabs/hadeth/hadeth_tab.dart
@@ -1,32 +1,11 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:islami_app/ui/home/tabs/hadeth/hadeeth_item.dart';
 
 class HadethTab extends StatelessWidget {
-
-  HadethTab({super.key});
+  const HadethTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery
-        .of(context)
-        .size
-        .height;
-    var width = MediaQuery
-        .of(context)
-        .size
-        .width;
     // TODO: implement build
-    return CarouselSlider(
-
-
-      options: CarouselOptions(height: height * .66,
-          enlargeCenterPage: true,
-          enableInfiniteScroll: false),
-      items: List.generate(50, (index) => index + 1).map((index) {
-        return HadeethItem(index: index);
-      }).toList(),
-    );
+    return Placeholder(color: Colors.red);
   }
-
 }
